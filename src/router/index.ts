b1d4f5373cc7598e53fecafc448a903d91f75e38
Caleb Rogers <caleb@calebjay.com>
2024-09-05 14:45:06 +0800

    Auth from store

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,24 @@ import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import { useAuthStore } from '../stores/auth';
 
+export enum ROUTES {
+  'home' = 'home',
+  'about' = 'about',
+  'login' = 'login'
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: ROUTES.home,
       component: HomeView,
-      meta: { requiresAuth: true },
-
+      meta: { requiresAuth: true }
     },
     {
       path: '/about',
-      name: 'about',
+      name: ROUTES.about,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
@@ -23,7 +28,7 @@ const router = createRouter({
     },
     {
       path: '/login',
-      name: 'login',
+      name: ROUTES.login,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
@@ -32,12 +37,11 @@ const router = createRouter({
   ]
 });
 
-
 router.beforeEach((to) => {
   // ✅ This will work because the router starts its navigation after
   // the router is installed and pinia will be installed too
-  const authStore = useAuthStore()
+  const authStore = useAuthStore();
 
-  if (to.meta.requiresAuth && !authStore.isLoggedIn) return '/login'
-})
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) return '/login';
+});
 export default router;
